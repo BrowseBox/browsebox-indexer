7118ba34f75e1311a095097aadec5a7d27d1fd45
Anusone Soula <anusone.soula@edu.sait.ca>
2023-04-03 15:35:51 -0600

    Add try/catch to all endpoints

--- src/Server.js
+++ src/Server.js
@@ -30,165 +30,177 @@ app.use(express.json());
 app.use(cors());
 
 app.post('/api/image/upload', upload.single('image'), async (req, res) => {
-    console.log("Received request to upload image!")
-    console.log("Extracting information from the request...")
-    const type = req.body.type
-    const id = req.body.id
-    const index = req.body.index
-    const file = req.file
+    try {
+        console.log("Received request to upload image!")
+        console.log("Extracting information from the request...")
+        const type = req.body.type
+        const id = req.body.id
+        const index = req.body.index
+        const file = req.file
 
-    console.log(`\tImage type: ${type}`)
-    console.log(`\tID: ${id}`)
-    console.log(`\tIndex: ${index}`)
-    console.log(`\tFile: ${file.originalname}`)
+        console.log(`\tImage type: ${type}`)
+        console.log(`\tID: ${id}`)
+        console.log(`\tIndex: ${index}`)
+        console.log(`\tFile: ${file.originalname}`)
 
-    console.log("Generating image key and image hash...")
-    const imageHash = crypto.createHash('sha256').update(file.buffer).digest('hex')
-    const fileBuffer = await sharp(file.buffer).toBuffer()
+        console.log("Generating image key and image hash...")
+        const imageHash = crypto.createHash('sha256').update(file.buffer).digest('hex')
+        const fileBuffer = await sharp(file.buffer).toBuffer()
 
-    if (type === "profile") {
-        console.log("Creating profile request to S3")
-        const key = 'assets/img/profile/' + imageHash.toString().substring(0, 1) + '/' + imageHash.toString().substring(0, 2) + '/' + imageHash + '.' + file.mimetype.substring(6)
+        if (type === "profile") {
+            console.log("Creating profile request to S3")
+            const key = 'assets/img/profile/' + imageHash.toString().substring(0, 1) + '/' + imageHash.toString().substring(0, 2) + '/' + imageHash + '.' + file.mimetype.substring(6)
 
-        console.log("Checking if profile already exists...")
-        const profileCheck = await prisma.profile.findUnique({
-            where: { userId: parseInt(id) }
-        })
-
-        if (profileCheck !== null) {
-            res.status(500)
-            console.log("Profile already exists. Aborting.")
-            return
-        } else {
-            const post = await prisma.profile.create({
-                data: { userId: parseInt(id), image: key, }
+            console.log("Checking if profile already exists...")
+            const profileCheck = await prisma.profile.findUnique({
+                where: { userId: parseInt(id) }
             })
-            await uploadFile(fileBuffer, key, file.mimetype)
 
-            res.status(200)
-            console.log("Image upload complete.")
+            if (profileCheck !== null) {
+                res.status(500)
+                console.log("Profile already exists. Aborting.")
+                return
+            } else {
+                const post = await prisma.profile.create({
+                    data: { userId: parseInt(id), image: key, }
+                })
+                await uploadFile(fileBuffer, key, file.mimetype)
+
+                res.status(200)
+                console.log("Image upload complete.")
+            }
         }
-    }
 
-    if (type === "listing") {
-        console.log("Creating listing request to S3")
-        const key = 'assets/img/listing/' + imageHash.toString().substring(0, 1) + '/' + imageHash.toString().substring(0, 2) + '/' + imageHash + '.' + file.mimetype.substring(6)
+        if (type === "listing") {
+            console.log("Creating listing request to S3")
+            const key = 'assets/img/listing/' + imageHash.toString().substring(0, 1) + '/' + imageHash.toString().substring(0, 2) + '/' + imageHash + '.' + file.mimetype.substring(6)
 
-        console.log("Checking if listing already exists...")
-        const listingCheck = await prisma.listing.findUnique({
-            where: { listingId: parseInt(id) }
-        })
-
-        if (listingCheck != null) {
-            res.status(500)
-            return
-        } else {
-            const post = await prisma.listing.create({
-                data: { listingId: parseInt(id), index: parseInt(index), image: key, }
+            console.log("Checking if listing already exists...")
+            const listingCheck = await prisma.listing.findUnique({
+                where: { listingId: parseInt(id) }
             })
-            await uploadFile(fileBuffer, key, file.mimetype)
 
-            res.status(200)
-            console.log("Image upload complete.")
+            if (listingCheck != null) {
+                res.status(500)
+                return
+            } else {
+                const post = await prisma.listing.create({
+                    data: { listingId: parseInt(id), index: parseInt(index), image: key, }
+                })
+                await uploadFile(fileBuffer, key, file.mimetype)
+
+                res.status(200)
+                console.log("Image upload complete.")
+            }
         }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
     }
 })
 
 app.post('/api/image/update', upload.single('image'), async (req, res) => {
-    const type = req.body.type
-    const id = req.body.id
-    const index = req.body.index
-    const file = req.file
+    try {
+        const type = req.body.type
+        const id = req.body.id
+        const index = req.body.index
+        const file = req.file
 
-    const imageHash = crypto.createHash('sha256').update(file.buffer).digest('hex')
-    const fileBuffer = await sharp(file.buffer).toBuffer()
+        const imageHash = crypto.createHash('sha256').update(file.buffer).digest('hex')
+        const fileBuffer = await sharp(file.buffer).toBuffer()
 
-    if (type === "profile") {
-        const key = 'assets/img/profile/' + imageHash.toString().substring(0, 1) + '/' + imageHash.toString().substring(0, 2) + '/' + imageHash + '.' + file.mimetype.substring(6)
+        if (type === "profile") {
+            const key = 'assets/img/profile/' + imageHash.toString().substring(0, 1) + '/' + imageHash.toString().substring(0, 2) + '/' + imageHash + '.' + file.mimetype.substring(6)
 
-        const oldKey = await prisma.profile.findUnique({
-            where: { userId: parseInt(id) }
-        })
+            const oldKey = await prisma.profile.findUnique({
+                where: { userId: parseInt(id) }
+            })
 
-        const post = await prisma.profile.update({
-            where: { userId: parseInt(id), },
-            data: { image: key, }
-        })
-        await uploadFile(fileBuffer, key, file.mimetype)
-        await deleteFile(oldKey.image)
+            const post = await prisma.profile.update({
+                where: { userId: parseInt(id), },
+                data: { image: key, }
+            })
+            await uploadFile(fileBuffer, key, file.mimetype)
+            await deleteFile(oldKey.image)
 
-        res.status(200)
+            res.status(200)
 
-    if (type === "listing") {
-        const key = 'assets/img/listing/' + imageHash.toString().substring(0, 1) + '/' + imageHash.toString().substring(0, 2) + '/' + imageHash + '.' + file.mimetype.substring(6)
+            if (type === "listing") {
+                const key = 'assets/img/listing/' + imageHash.toString().substring(0, 1) + '/' + imageHash.toString().substring(0, 2) + '/' + imageHash + '.' + file.mimetype.substring(6)
 
-        const post = await prisma.listing.update({
-            where: { listingId: parseInt(id) },
-            data: { index: parseInt(index), }
-        })
-        await uploadFile(fileBuffer, key, file.mimetype)
+                const post = await prisma.listing.update({
+                    where: { listingId: parseInt(id) },
+                    data: { index: parseInt(index), }
+                })
+                await uploadFile(fileBuffer, key, file.mimetype)
 
-        res.status(200)
-    }
+                res.status(200)
+            }
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
     }
 })
 
 app.post("/api/image/delete", upload.single('image'), async (req, res) => {
-    const type = req.body.type
-    const id = req.body.id
-    const index = req.body.index
+    try {
+        switch (type) {
+            case "profile":
+                const profile = await prisma.profile.findUnique({
+                    where: { userId: parseInt(id) }
+                })
 
-    switch (type) {
-        case "profile":
-            const profile = await prisma.profile.findUnique({
-                where: { userId: parseInt(id) }
-            })
+                await prisma.profile.delete({
+                    where: { userId: parseInt(id) }
+                })
+                await deleteFile(profile.image)
 
-            await prisma.profile.delete({
-                where: { userId: parseInt(id) }
-            })
-            await deleteFile(profile.image)
+                res.status(200)
+                break;
 
-            res.status(200)
-            break;
+            case "listing":
+                const listing = await prisma.listing.findFirst({
+                    where: { listingId: parseInt(id), index: parseInt(index) }
+                })
 
-        case "listing":
-            const listing = await prisma.listing.findFirst({
-                where: { listingId: parseInt(id), index: parseInt(index) }
-            })
+                await prisma.listing.deleteMany({
+                    where: { listingId: parseInt(id), index: parseInt(index) }
+                })
+                await deleteFile(listing.image)
 
-            await prisma.listing.deleteMany({
-                where: { listingId: parseInt(id), index: parseInt(index) }
-            })
-            await deleteFile(listing.image)
-
-            res.status(200)
-            break;
+                res.status(200)
+                break;
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
     }
 })
 
 // get request returns image hash
 app.get('/api/image/retrieve/:type/:id/:index', async (req, res) => {
-    const type = req.params.type
-    const id = req.params.id
-    const index = req.params.index
+    try {
+        const type = req.params.type
+        const id = req.params.id
+        const index = req.params.index
 
-    switch (type) {
-        case "profile":
-            const profile = await prisma.profile.findUnique({
-                where: { userId: parseInt(id) }
-            })
+        switch (type) {
+            case "profile":
+                const profile = await prisma.profile.findUnique({
+                    where: { userId: parseInt(id) }
+                })
 
-            res.status(200).send(profile.image)
-            break;
+                res.status(200).send(profile.image)
+                break;
 
-        case "listing":
-            const listing = await prisma.listing.findFirst({
-                where: { listingId: parseInt(id), index: parseInt(index) }
-            })
+            case "listing":
+                const listing = await prisma.listing.findFirst({
+                    where: { listingId: parseInt(id), index: parseInt(index) }
+                })
 
-            res.status(200).send(listing.image)
-            break;
+                res.status(200).send(listing.image)
+                break;
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Internal server error' });
     }
 })
 
