327b6721a8e09f2a156abf73dedb340412cd06f7
asoula21 <anusone.soula@edu.sait.ca>
2023-04-04 15:28:06 -0600

    Split GET endpoints to seperate profile/listing

--- src/Server.ts
+++ src/Server.ts
@@ -396,76 +396,98 @@ app.post("/api/image/delete", upload.single('image'), async (req, res) => {
 });
 
 /**
- * @route GET /api/image/retrieve/:type/:id/:index
- * @desc Retrieve the image hash for a user profile or listing.
- * @param {string} type - The image type, either "profile" or "listing".
- * @param {number} id - The user ID for profiles or listing ID for listings.
- * @param {number} index - The order of the image for listing images. Only required for listing images.
+ * @route GET /api/image/retrieve/profile/:id/
+ * @desc Retrieve the image hash for a user profile.
+ * @param {number} id - The user ID.
  * @returns {string} imageHash - The hash of the requested image.
  */
-app.get('/api/image/retrieve/:type/:id/:index', async (req, res) => {
+app.get('/api/image/retrieve/profile/:id', async (req, res) => {
     let profile;
-    let listing;
+    let key;
 
     try {
         log("Received API request to retrieve image.");
         log("Extracting information from the request...");
-        const type = req.params.type;
+        const id = req.params.id;
+
+        log(padText("Image type:", 16) + "profile");
+        log(padText("ID:", 16) + id);
+
+        log("Retrieving profile image...");
+        profile = await prisma.profile.findUnique({
+            where: {
+                userId: parseInt(id)
+            }
+        }).catch(error => {
+            log("Database error while fetching profile: " + error.message, LogLevel.ERROR);
+            res.status(500).json({ message: "Database error while fetching profile." });
+            return;
+        });
+
+        if (profile) {
+            key = profile.image;
+            log(`Image Key: ${key}`);
+            log("Profile image retrieved.");
+        } else {
+            res.status(500).json({ message: "Failed to retrieve profile from database." });
+            log("Failed to retrieve profile from database.", LogLevel.ERROR);
+        }
+
+        const url = "https://" + process.env.S3_BUCKET + ".s3.us-west-2.amazonaws.com/" + key;
+        log("Sending image URL to client.");
+        res.status(200).json({ imageUrl: url });
+        log("Image URL: " + url);
+    } catch (error) {
+        if (error instanceof Error) {
+            res.status(500).json({
+                message: 'Internal server error'
+            });
+
+            log("Internal server error: " + error.message, LogLevel.ERROR);
+        }
+    }
+});
+
+/**
+ * @route GET /api/image/retrieve/listing/:id/:index
+ * @desc Retrieve the image hash for a listing.
+ * @param {number} id - The listing ID.
+ * @param {number} index - The order of the image for listing images.
+ * @returns {string} imageHash - The hash of the requested image.
+ */
+app.get('/api/image/retrieve/listing/:id/:index', async (req, res) => {
+    let listing;
+    let key;
+
+    try {
+        log("Received API request to retrieve image.");
+        log("Extracting information from the request...");
         const id = req.params.id;
         const index = req.params.index;
 
-        log(padText("Image type:", 16) + type);
+        log(padText("Image type:", 16) + "listing");
         log(padText("ID:", 16) + id);
         log(padText("Index:", 16) + index);
 
-        let key;
-        switch (type) {
-            case "profile":
-                log("Retrieving profile image...");
-                profile = await prisma.profile.findUnique({
-                    where: {
-                        userId: parseInt(id)
-                    }
-                }).catch(error => {
-                    log("Database error while fetching profile: " + error.message, LogLevel.ERROR);
-                    res.status(500).json({ message: "Database error while fetching profile." });
-                    return;
-                });
+        log("Retrieving listing image...");
+        listing = await prisma.listing.findFirst({
+            where: {
+                listingId: parseInt(id),
+                index: parseInt(index)
+            }
+        }).catch(error => {
+            log("Database error while fetching listing: " + error.message, LogLevel.ERROR);
+            res.status(500).json({ message: "Database error while fetching listing." });
+            return;
+        });
 
-                if (profile) {
-                    key = profile.image;
-                    log(`Image Key: ${key}`);
-                    log("Profile image retrieved.");
-                    break;
-                } else {
-                    res.status(500).json({ message: "Failed to retrieve profile from database." });
-                    log("Failed to retrieve profile from database.", LogLevel.ERROR);
-                    break;
-                }
-
-            case "listing":
-                log("Retrieving listing image...");
-                listing = await prisma.listing.findFirst({
-                    where: {
-                        listingId: parseInt(id),
-                        index: parseInt(index)
-                    }
-                }).catch(error => {
-                    log("Database error while fetching listing: " + error.message, LogLevel.ERROR);
-                    res.status(500).json({ message: "Database error while fetching listing." });
-                    return;
-                });
-
-                if (listing) {
-                    key = listing.image;
-                    log(`Image Key: ${key}`);
-                    log("Listing image retrieved.");
-                    break;
-                } else {
-                    res.status(500).json({ message: "Failed to retrieve listing from database." });
-                    log("Failed to retrieve listing from database.", LogLevel.ERROR);
-                    break;
-                }
+        if (listing) {
+            key = listing.image;
+            log(`Image Key: ${key}`);
+            log("Listing image retrieved.");
+        } else {
+            res.status(500).json({ message: "Failed to retrieve listing from database." });
+            log("Failed to retrieve listing from database.", LogLevel.ERROR);
         }
 
         const url = "https://" + process.env.S3_BUCKET + ".s3.us-west-2.amazonaws.com/" + key;
