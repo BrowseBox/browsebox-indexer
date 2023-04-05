7fa04d1107b8e1286a1c460ca935fbb4482fbfe3
asoula21 <anusone.soula@edu.sait.ca>
2023-04-05 17:09:29 -0600

    Fix logger overwriting the same file

--- src/utils/Logger.ts
+++ src/utils/Logger.ts
@@ -1,12 +1,15 @@
 // Copyright (c) BrowseBox. Licensed under the MIT Licence.
 // See the LICENCE file in the repository root for full licence text.
 
-// Define LogLevel enum
+import fs from 'fs';
+
 enum LogLevel {
     VERBOSE = 'verbose',
     WARNING = 'warning',
     ERROR = 'error',
 }
+
+const now = new Date();
 
 /**
  * Logs a message with a specified log level and timestamp.
@@ -14,13 +17,56 @@ enum LogLevel {
  * @param {LogLevel} level - The log level (default: LogLevel.VERBOSE).
  */
 const log = (message: string, level: LogLevel = LogLevel.VERBOSE): void => {
-    const now = new Date();
-    const localTimestamp: string = now.toLocaleString('en-US', {
-        hour12: false, timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone
+    const localDate = now.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+    }).split('/').reverse().join('-');
+
+    const localTimeStamp = now.toLocaleTimeString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        hour12: false,
     });
-    console.log(`${localTimestamp} [${level}]: ${message}`);
+
+    const logMessage: string = `${localDate} ${localTimeStamp} [${level}]: ${message}`;
+    console.log(logMessage);
+
+    const logDirectory = './logs/';
+    if (!fs.existsSync(logDirectory)) {
+        fs.mkdirSync(logDirectory);
+    }
+
+    const logFileName = getLogFileName();
+    fs.appendFile(logDirectory + logFileName, logMessage, (err) => {
+        if (err) {
+            console.error(`Error writing to log file: ${err.message}.`);
+        }
+    });
 };
 
+/**
+ * Returns the log file name in the format "YYYY-MM-DD-N.log" where "N" is a unique log number.
+ * @returns {string} The log file name.
+ */
+function getLogFileName(): string {
+    const localDate = now.toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit',
+    }).split('/').reverse().join('-');
+
+    let logNumber = 0;
+    let logFileName = `${localDate}-${logNumber}.log`;
+
+    while (fs.existsSync('./logs/' + logFileName)) {
+        logNumber++;
+        logFileName = `${localDate}-${logNumber}.log`;
+    }
+
+    return logFileName;
+}
 
 /**
  * Pads a text string with spaces on the right side up to the specified length.
